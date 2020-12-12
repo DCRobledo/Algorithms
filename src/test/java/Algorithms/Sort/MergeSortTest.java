d1b75efb91f3fcac54523f2e6bbaf4c1b1f5ee62
DCRobledo <daniel.canorobledo@gmail.com>
2020-12-12 13:32:47 +0100

    Tests Structures Prepared

--- src/test/java/Algorithms/Sort/MergeSortTest.java
+++ src/test/java/Algorithms/Sort/MergeSortTest.java
@@ -3,10 +3,14 @@ import org.junit.Test;
 import org.junit.Before;    
     
 public class MergeSortTest {
+    MergeSort ms;
+
+    int array[] = {1, 4, 2, 7, 4, -10, 20, -3};
+    int expected[] = {-10, -3, 1, 2, 4, 4, 7, 20};
 
     @Before
     public void setup(){
-
+        ms = new MergeSort();
     }
         
     @Test
--- src/test/java/Algorithms/Sort/QuickSortTest.java
+++ src/test/java/Algorithms/Sort/QuickSortTest.java
@@ -3,10 +3,14 @@ import org.junit.Test;
 import org.junit.Before;    
     
 public class QuickSortTest {
+    QuickSort qs;
+
+    int array[] = {1, 4, 2, 7, 4, -10, 20, -3};
+    int expected[] = {-10, -3, 1, 2, 4, 4, 7, 20};
 
     @Before
     public void setup(){
-
+        qs = new QuickSort();
     }
         
     @Test
