7f04027c645cc6139ac823ef4478454b6ea696f7
DCRobledo <daniel.canorobledo@gmail.com>
2020-12-12 13:29:19 +0100

    Selection Sort Implemented. Tests Passed

--- src/main/java/Algorithms/Sort/SelectionSort.java
+++ src/main/java/Algorithms/Sort/SelectionSort.java
@@ -1,5 +1,22 @@
 package Algorithms.Sort;
 
 public class SelectionSort {
-    
+    public int getMinIndex(int[] array, int startPosition) {
+        int minIndex = startPosition;
+
+        for (int i = startPosition; i < array.length; i++)
+            if(array[i] < array[minIndex]) minIndex = i;
+
+        return minIndex;
+    }
+
+    public void selectionSort(int [] array) {
+        for(int sorted = 0; sorted < array.length; sorted++) {
+            int minIndex = getMinIndex(array, sorted);
+
+            int temp = array[sorted];
+            array[sorted] = array[minIndex];
+            array[minIndex] = temp;
+        }
+    }
 }
--- src/test/java/Algorithms/Sort/SelectionSortTest.java
+++ src/test/java/Algorithms/Sort/SelectionSortTest.java
@@ -1,17 +1,26 @@
 package Algorithms.Sort;
 import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
 import org.junit.Before;    
     
 public class SelectionSortTest {
+    SelectionSort ss;
+
+    int array[] = {1, 4, 2, 7, 4, -10, 20, -3};
+    int expected[] = {-10, -3, 1, 2, 4, 4, 7, 20};
 
     @Before
     public void setup(){
-
+        ss = new SelectionSort();
     }
         
     @Test
-    public void test() {
-        
+    public void selectionSortTest() {
+        ss.selectionSort(array);
+
+        assertArrayEquals(expected, array);
     }
 }
     
