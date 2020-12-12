fdfd48ef85ac38ff533ed40c2e21b0f78d017b99
DCRobledo <daniel.canorobledo@gmail.com>
2020-12-12 13:46:21 +0100

    Bubble Sort Implemented. Tests Passed

--- src/test/java/Algorithms/Sort/BubbleSortTest.java
+++ src/test/java/Algorithms/Sort/BubbleSortTest.java
@@ -1,17 +1,26 @@
 package Algorithms.Sort;
 import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
 import org.junit.Before;    
     
 public class BubbleSortTest {
+    BubbleSort bs;
+
+    int array[] = {1, 4, 2, 7, 4, -10, 20, -3};
+    int expected[] = {-10, -3, 1, 2, 4, 4, 7, 20};
 
     @Before
     public void setup(){
-
+        bs = new BubbleSort();
     }
         
     @Test
-    public void test() {
-        
+    public void bubbleSortTest() {
+        bs.bubbleSort(array);
+
+        assertArrayEquals(expected, array);
     }
 }
     
