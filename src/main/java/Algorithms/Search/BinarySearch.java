2b9b109ccefbb867c393fc0c291bb5d471aff404
DCRobledo <daniel.canorobledo@gmail.com>
2020-12-29 13:23:19 +0100

    Binary Search Implemented. PASS

--- src/main/java/Algorithms/Search/BinarySearch.java
+++ src/main/java/Algorithms/Search/BinarySearch.java
@@ -1,5 +1,18 @@
 package Algorithms.Search;
 
 public class BinarySearch {
-    
+    private int binarySearch(int[] array, int target, int left, int right) {
+        int middle = (right + left) / 2;
+
+        if (array[middle] == target) return middle;
+
+        if (left >= right) return -1;
+
+        if (array[middle] > target) return binarySearch(array, target, left, middle - 1);
+        return binarySearch(array, target, middle + 1, right);
+    }
+
+    public int binarySearch(int[] array, int target) {
+        return binarySearch(array, target, 0, array.length - 1);
+    }
 }
--- src/test/java/Algorithms/Search/BinarySearchTest.java
+++ src/test/java/Algorithms/Search/BinarySearchTest.java
@@ -1,17 +1,28 @@
 package Algorithms.Search;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;    
     
 public class BinarySearchTest {
+    BinarySearch bs;
+
+    public int[] array = {-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8};
 
     @Before
     public void setup(){
-
+        bs = new BinarySearch();
     }
         
     @Test
-    public void test() {
-        
+    public void binarySearchCorrectTest() {
+        assertEquals(2, bs.binarySearch(array, -1));
+    }
+
+    @Test
+    public void binarySearchWrongTest() {
+        assertEquals(-1, bs.binarySearch(array, 10));
     }
 }
     
